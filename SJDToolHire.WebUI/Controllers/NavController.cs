a8c34ca20910e6d438e82df2efb9d8ddd8bac7e1
spen440 <spenjd@hotmail.co.uk>
2014-11-05 17:19:36 +0000

    Interim commit.

---
+++ SJDToolHire.WebUI/Controllers/NavController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SJDToolHire.Domain.Abstract;
+
+namespace SJDToolHire.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IToolRepository repository;
+
+        public NavController(IToolRepository repo)
+        {
+            repository = repo;
+        }
+
+        public PartialViewResult Menu(string category = null)
+        {
+            ViewBag.SelectedCategory = category;
+            IEnumerable<string> categories = repository.Tools
+                                             .Select(x => x.Category)
+                                             .Distinct()
+                                             .OrderBy(x => x);
+            return PartialView(categories);
+        }
+    }
+}
