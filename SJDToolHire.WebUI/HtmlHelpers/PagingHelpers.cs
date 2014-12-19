43b5397bb80fb09380f6fb90b5fd6f2473e199d9
spen440 <spenjd@hotmail.co.uk>
2014-12-19 20:31:54 +0000

    Interim Commit.

--- SJDToolHire.WebUI/HtmlHelpers/PagingHelpers.cs
+++ SJDToolHire.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -18,12 +18,16 @@ namespace SJDToolHire.WebUI.HtmlHelpers
                 TagBuilder tag = new TagBuilder("a");
                 tag.MergeAttribute("href", pageUrl(i));
                 tag.InnerHtml = i.ToString();
+                tag.AddCssClass("btn tn-default");
                 if (i == pagingInfo.CurrentPage)
                 {
                     tag.AddCssClass("selected");
-                    tag.AddCssClass("btn-primary");
+                    tag.MergeAttribute("style", "background-color: #000000; border-color: #DEDE28; color: #DEDE28;");
                 }
-                tag.AddCssClass("btn btn-default");
+                else
+                {
+                    tag.MergeAttribute("style", "background-color: #DEDE28; border-color: #000000; color: #000000;");
+                }
                 result.Append(tag.ToString());
             }
             return MvcHtmlString.Create(result.ToString());
